b52da79da72176784a416a574103a409f2e215dd
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-12-23 16:16:36 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+import data
+import os
+
+def play_game():
+    score = 0
+    in_session = True
+    while in_session:
+        choice_a = random.choice(data.data)
+        choice_b=random.choice(data.data)
+
+        
+        print(f'Score: {score}')
+        print(f'compare A: {choice_a["name"]}: {choice_a["description"]} from: {choice_a["country"]} ')
+        print('VS')
+        print(f'compare B: {choice_b["name"]}: {choice_b["description"]} from: {choice_b["country"]}')
+        answer = max(choice_a["follower_count"],choice_b["follower_count"])
+
+
+        user_pick = input('Who has more followers A or B: ')
+        if user_pick == 'a':
+            pick = choice_a["follower_count"]
+        else:
+            pick = choice_b["follower_count"]
+        if pick == answer:
+            score+=1
+            print('correct')
+            os.system('cls')
+        else:
+            in_session = False
+            print(f'wrong you lose final score: {score}')
+    
+
+play_game()
